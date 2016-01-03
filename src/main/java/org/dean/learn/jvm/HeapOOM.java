9d569dfa8de222dccb76dc0e38da6b86deff3e84
Dean <245405244@qq.com>
2016-01-03 11:13:01 +0800

    提交堆内存溢出异常测试类

---
+++ src/main/java/org/dean/learn/jvm/HeapOOM.java
@@ -0,0 +1,23 @@
+package org.dean.learn.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Arags: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * Created by Dean on 2016/1/1.
+ */
+public class HeapOOM {
+
+    static class OOMObject{
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        while (true){
+            list.add(new OOMObject());
+        }
+        //运行即会报内存溢出异常
+    }
+
+}
